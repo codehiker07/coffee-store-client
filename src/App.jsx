e37dd4ecf0909a61c1b076db6f114eb4f6941228
codehiker07 <codehiker07@gmail.com>
2023-12-15 17:19:59 +0600

    Coffee data added form mongoDB server to client side

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import CoffeeCard from "./components/CoffeeCard";
 function App() {
   const coffees = useLoaderData();
   return (
-    <div className="m-20">
+    <div className="max-w-max mx-auto">
       <h1 className="text-6xl text-center my-20 text-purple-600 font-bold">
         Hot Hot Cold Coffee : {coffees.length}
       </h1>
-      <div className="grid md:grid-cols-2 gap-4">
+      <div className="grid md:grid-cols-2 gap-6">
         {coffees.map((coffee) => (
           <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>
         ))}
--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -2,16 +2,18 @@ const CoffeeCard = ({ coffee }) => {
   const { name, quantity, supplier, test, category, details, photo } = coffee;
   return (
     <div>
-      <div className="card card-side bg-base-100 shadow-xl">
+      <div className="card card-side p-8 bg-base-100 shadow-xl">
         <figure>
-          <img src={photo} alt="photo" />
+          <img className="w-36 h-full" src={photo} alt="photo" />
         </figure>
         <div className="flex justify-between w-full p-4">
           <h2 className="card-title">Name: {name}</h2>
-          <p>{details}</p>
-          <p>{quantity}</p>
-          <p>{supplier}</p>
-          <p>{test}</p>
+          <div className="grid grid-cols-1">
+            <p>{details}</p>
+            <p>{quantity}</p>
+            <p>{supplier}</p>
+            <p>{test}</p>
+          </div>
           <div className="card-actions justify-end">
             <div className="join join-vertical space-y-4">
               <button className="btn join-item">View</button>
